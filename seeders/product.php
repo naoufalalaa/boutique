d935ff0541ef20746a4964d1e349af9d537028f8
naoufalalaa <alaanaoufal2000@gmail.com>
2021-06-11 18:44:35 +0100

    commit

--- seeders/product.php
+++ seeders/product.php
@@ -5,10 +5,10 @@ $ini = json_decode($json,true);
 foreach($ini as $row){
     $array=$row['category'];
         $rows.= $row['sku'].' => '.$row['name'].' => '.$row['description'].' => '.$row['price'].' => '.$array[0]['id'].' => '.$array[0]['name'].'<br>';
-        // $insert = $bdd->prepare('INSERT INTO products(sku,name,description,price,image,catID)VALUES(?,?,?,?,?,?)');
-        // $insert->execute(array($row['sku'],$row['name'],$row['description'],$row['price'],$row['image'],$array[0]['id']));
-        // $insertCat = $bdd->prepare('INSERT INTO categorie (id, name)VALUES (?,?)');
-        // $insertCat->execute(array($array[0]['id'],$array[0]['name']));
+        $insert = $bdd->prepare('INSERT INTO products(sku,name,description,price,image,catID)VALUES(?,?,?,?,?,?)');
+        $insert->execute(array($row['sku'],$row['name'],$row['description'],$row['price'],$row['image'],$array[0]['id']));
+        $insertCat = $bdd->prepare('INSERT INTO categorie (id, name)VALUES (?,?)');
+        $insertCat->execute(array($array[0]['id'],$array[0]['name']));
 }
 ?>
 <!DOCTYPE html>
