1df1da612ac890e347d50841d8ae57fedf887306
naoufalalaa <alaanaoufal2000@gmail.com>
2021-06-12 11:41:03 +0100

    commit

--- index.php
+++ index.php
@@ -7,7 +7,28 @@ include_once './controllers/login.php';
 include_once './controllers/signin.php';
 ?>
 
-<script>home()</script>
+<script>
+home();
+// $(document).ready(function()
+// {
+//     $('#add').click(function()
+//     {
+//         var id = $('#id').val();
+//         var idUser = $('#user').val();
+//         var qte = $('#qte').val();
+
+//         $.ajax({
+//             url: './controllers/addtocart.php',
+//             type: 'POST',
+//             data: { id:id, user:idUser, qte:qte },
+//             success: function(data)
+//             {
+//                 alert('Article added to cart!');
+//             }
+//         })
+//     });
+// });
+</script>
 <div id="root" align="center">
 </div>
 <div align='center' id="ro">
@@ -26,24 +47,49 @@ include_once './controllers/signin.php';
         <li style="float: left;"><a href="index.php?page=<?=$pageCourante-1?>"><span uk-pagination-previous></span> Page Precedente</a></li>
         <li style="float: right;"><a href="index.php?page=<?=$pageCourante+1?>">Page Suivante<span uk-pagination-next></span> </a></li>
     </ul>
-    <div align='left' id="divis" class="uk-grid-match uk-width-6-7 uk-child-width-1-2@s uk-child-width-1-6@l uk-text-center" uk-grid="parallax: 150">
+    <div align='left' id="divis" class="uk-grid-match uk-width-6-7 uk-child-width-1-2@s uk-child-width-1-5@l uk-text-center" uk-grid="parallax: 150">
         <?php if($checker>0){ while($a=$article->fetch()){ ?>
             <div>
                 <div class="uk-box-shadow-hover-large uk-card uk-card-default">
                     <div class="uk-card-media-top uk-cover-container">
-                        <img src="<?=$a['image']?>" alt="<?=$a['name']?>">
-                        <canvas width="" height="100px"></canvas>
+                        <img src="<?=$a['image']?>" width="200px" height="100px" alt="<?=$a['name']?>">
                     </div>
                     <div class="uk-card-body">
                         <h5><?=$a['name'] ?></h5>
                         <small id="ar<?=$a['sku'] ?>"></small>
                     </div>
                     <div class="uk-card-footer">
-                        <?=$a['price'] ?>$
+                    <a class="uk-button uk-button-secondary" href="#modal-full<?=$a['sku']?>" uk-toggle><small>Add to cart</small></a>    
+                    <br> <?=$a['price'] ?>$
                     </div>
                 </div>
             </div>
         
+
+
+
+
+
+            <div id="modal-full<?=$a['sku']?>" class="uk-modal-full" uk-modal>
+                <div class="uk-modal-dialog">
+                    <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
+                    <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
+                        <div class="uk-background-cover" style="background-image: url(<?=$a['image']?>);" uk-height-viewport></div>
+                        <div class="uk-padding-large">
+                            <h1><?=$a['name'] ?></h1>
+                            <p><?=$a['description'] ?></p>
+                            <form method="post">    
+                            <div class="uk-width-2-3@s">
+                                <input required class="uk-input" name="qte" type="number" placeholder="Quantity">
+                            </div> 
+                            <input required name="id" type="hidden" value="<?=$a['sku'] ?>">
+                            <input required type="hidden" value="<?=$_SESSION['id'] ?>">
+                            <button class="uk-button uk-button-secondary" type="submit">Add</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         <?php  }} ?>
     </div>
     <ul class="uk-pagination" uk-margin>
